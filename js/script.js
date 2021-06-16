2c4ff34a40a2232737e3d70d36de1c4b39f880cf
SayanSenedwne <78363927+SayanSenedwne@users.noreply.github.com>
2021-06-17 00:58:10 +0600

    Residential-complex
    Residential-complex

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock')
+    });
+});
+
+$(document).ready(function () {
+    $('.header__link').click(function (event) {
+        $('.header__burger, .header__menu').removeClass('active');
+        $('body').removeClass('lock')
+    });
+});
+
+
+const animItems = document.querySelectorAll('._anim-items');
+
+
+if(animItems. length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active'); 
+            } else {
+                animItem.classList.remove('_active');
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect + scrollLeft}
+    }
+    setTimeout(() => {
+       animOnScroll(); 
+    } ,300);
+}
+
